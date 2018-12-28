3300518150b06c5e418afca790a83c8f0d5d152e
Givailson de Souza Neves <givasneves@MacBook-Pro-de-Givailson.local>
2018-12-28 09:24:41 -0200

    Dev - inserção da api de testes para a recyclerview

--- app/src/androidTest/java/com/example/givasneves/receitasculinarias/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/example/givasneves/receitasculinarias/MainActivityScreenTest.java
@@ -1,5 +1,9 @@
 package com.example.givasneves.receitasculinarias;
 
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.ActivityTestRule;
 
@@ -7,35 +11,45 @@ import com.example.givasneves.receitasculinarias.adapter.RecipeListAdapter;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.anything;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onData;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityScreenTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
+
+    private static final Intent MY_ACTIVITY_INTENT = new Intent(InstrumentationRegistry.getTargetContext(), MainActivity.class);
+
+    private static int BROWNIE_POSITION = 1;
+    private static String BROWNIE_TEXT = "Brownies";
+
+    @Before
+    public void setup() {
+        mActivityTestRule.launchActivity(MY_ACTIVITY_INTENT);
+    }
 
     @Test
     public void clickGridViewItem_OpenOrderActivity() {
 
-        // gridview item and clicks it.
-        onData(instanceOf(RecipeListAdapter.class)).inAdapterView(withId(R.id.recipe_list_fragment)).atPosition(1).perform(click());
+        //executa o click no segundo item do recyclerview
+        onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(BROWNIE_POSITION,
+                        click()));
 
+        //Verifica se a nova tela aberta contem o item desejado
+        onView(withId(R.id.recipe_title)).check(matches(withText(BROWNIE_TEXT)));
 
-        // Verifica se ao entrar na tela o campo de titulo foi preenchido.
-       // onView(withId(R.id.recipe_title)).check(matches(not(withText(""))));
     }
 
 }
